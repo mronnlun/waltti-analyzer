4662b423c1c20c3d373eed171008dee4c42b4780
mathias.ronnlund <mathias.ronnlund@gmail.com>
2026-04-06 16:41:09 +0300

    Add Azure function examples

---
+++ api/example/ExampleFunctionApp/TimerTriggeredFunction.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExampleFunctionApp
+{
+    public class TimerTriggeredFunction
+    {
+        private readonly ILogger _logger;
+
+        public TimerTriggeredFunction(ILoggerFactory loggerFactory)
+        {
+            _logger = loggerFactory.CreateLogger<TimerTriggeredFunction>();
+        }
+
+        [Function("TimerTriggeredFunction")]
+        public void Run([TimerTrigger("0 */5 * * * *")] TimerInfo myTimer)
+        {
+            _logger.LogInformation("C# Timer trigger function executed at: {executionTime}", DateTime.Now);
+
+            if (myTimer.ScheduleStatus is not null)
+            {
+                _logger.LogInformation("Next timer schedule at: {nextSchedule}", myTimer.ScheduleStatus.Next);
+            }
+        }
+    }
+}
